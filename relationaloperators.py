ef6e7851fc81b2d2f837529f9668bd24fdf869d8
korpupavani <korpupavanikorpu@gmail.com>
2025-07-03 09:51:36 +0530

    Add files via upload

---
+++ relationaloperators.py
@@ -0,0 +1,14 @@
+"""
+relational/comparison operators are used to 
+compare the values 
+and return the boolean they are
+>,<,>=,==,!=
+"""
+a = int(input("enter a number:"))
+b = int(input("enter a number:"))
+print("the greater value ",a>b)
+print("the lasser value",a<b)
+print("the greater than or equals to value",a>=b)
+print("the less than or equals too value",a<=b)
+print("the equals too value ",a==b)
+print("the not equals too value ",a!=b)
