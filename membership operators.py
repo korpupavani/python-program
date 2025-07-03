f39cba4329ea3ac8b2e06e6a6196562b04e8c4b6
korpupavani <korpupavanikorpu@gmail.com>
2025-07-03 09:48:18 +0530

    Add files via upload

---
+++ membership operators.py
@@ -0,0 +1,11 @@
+"""
+membership operators are used to check the values in 
+a sequence
+and returns the boolean values
+they operators are "in","not in"
+"""
+x = ["apple","bannana"]
+print("apple"in x)
+print("pp not in x")
+print("bannana"not in x)
+print("dragon"in x)
