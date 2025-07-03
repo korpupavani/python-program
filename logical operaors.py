efccba8484a8de392452dc89e3a472164b9ad8e6
korpupavani <korpupavanikorpu@gmail.com>
2025-07-03 09:46:05 +0530

    Add files via upload

---
+++ logical operaors.py
@@ -0,0 +1,32 @@
+"""
+logical operators are used to perform logical operatons
+they are logicaland,or,not
+and--if both the conditions are true it returns the true
+T T T
+F T T
+T F F
+F F F
+or---if one of th codition is true it returns the true
+T F T
+F T T
+T T T
+F F F
+not--it just negotiates the condition
+T F
+F T
+"""
+a = 13
+b = 45
+c = a>=34 and b<=50
+print(c)
+
+d = 67
+e = 89
+f =d!=67 or e==89
+print(f)
+
+a = 10
+print(not(a))
+    
+b = 0
+print(not(b))
